391a7d012bc969fb82b5a6ed97af103c7c367c97
Siddharth Kar <sgworks.co@gmail.com>
2015-09-07 02:53:04 +0530

    Adding a util class of Json reponses - Siddharth

---
+++ src/main/java/com/sgworks/mobile/util/JsonResponseUtil.java
@@ -0,0 +1,28 @@
+package com.sgworks.mobile.util;
+
+public class JsonResponseUtil {
+
+    private String status = "";
+    private String errorMessage = "";
+
+    public JsonResponseUtil (String status, String errorMessage) {
+        this.status = status;
+        this.errorMessage = errorMessage;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
