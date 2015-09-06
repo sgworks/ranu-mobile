cc83a2e4e4faea010f786363b92067f307942652
Siddharth Kar <sgworks.co@gmail.com>
2015-09-07 02:52:22 +0530

    Adding Rest uri constants and Job Controller to take care of the uri mappings and job crud operations - Siddharth

---
+++ src/main/java/com/sgworks/mobile/controller/JobController.java
@@ -0,0 +1,13 @@
+package com.sgworks.mobile.controller;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = RanuMobileRestURIConstants.jobRootMappingURI)
+public class JobController {
+
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(JobController.class);
+
+}
---
+++ src/main/java/com/sgworks/mobile/controller/RanuMobileRestURIConstants.java
@@ -0,0 +1,10 @@
+package com.sgworks.mobile.controller;
+
+public class RanuMobileRestURIConstants {
+
+    // User Controller Rest URI Constants
+    public static final String userRootMappingURI = "/api/account";
+
+    // Job Controller Rest URI Constants
+    public static final String jobRootMappingURI = "/api/job";
+}
